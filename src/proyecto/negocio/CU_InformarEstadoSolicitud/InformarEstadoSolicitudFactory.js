64a50b99e624498cc90ade16de1c4ef2f87782d4
Federico Perchuk <fperchuk@hotmail.com>
2021-06-21 16:43:41 -0300

    Fix en dependencia mail factory

--- src/proyecto/negocio/CU_InformarEstadoSolicitud/InformarEstadoSolicitudFactory.js
+++ src/proyecto/negocio/CU_InformarEstadoSolicitud/InformarEstadoSolicitudFactory.js
@@ -1,5 +1,5 @@
 import CasoDeUso_InformarEstadoSolicitud from './CU_InformarEstadoSolicitud.js'
-import MailerFactory from '../../../compartidos/MailerFactory.js'
+import MailerFactory from '../../../compartidos/mail/mailFactory.js'
 import QRFactory from '../../../compartidos/QRFactory.js'
 import { crearDaoLocal } from '../../persistencia/daoLocal.js'
 import { crearDaoSolicitud } from '../../persistencia/daoSolicitud.js'
