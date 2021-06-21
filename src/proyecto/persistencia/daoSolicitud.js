9e013259ee7dfc0a86a5d876d4f14b33d0b8a659
Federico Brian Steinberg <fedesteinberg@gmail.com>
2021-06-21 20:42:53 -0300

    .

--- src/proyecto/persistencia/daoSolicitud.js
+++ src/proyecto/persistencia/daoSolicitud.js
@@ -1,7 +1,17 @@
 import { crearSolicitud } from "../modelos/Solicitud"
 
 const crearDaoSolicitud = () => {
+    const solicitudes = [
+        {
+            id: 0,
+            idLocal: 0,
+            urlArchivo: '',
+            estado: '',
+            fechaSolicitud: ''
+        }
+    ]
     const daoSolicitud = {
+
         getById: (idSolicitud) => {
             console.log('solicitud encontrada!')
             return { idSolicitud, idLocal:0, estado:'pendiente-rechazado', fechaSolicitud:'2021-05-02', motivoRechazo:'Información incompleta'}
@@ -10,9 +20,18 @@ const crearDaoSolicitud = () => {
             const nuevaSolicitud = {}
             nuevaSolicitud.urlArchivo = urlArchivo
             nuevaSolicitud.fechaSolicitud = Date.now()
-            nuevaSolicitud.idSolicitud = 0
+            nuevaSolicitud.id = 0
             nuevaSolicitud.idLocal = 0
             nuevaSolicitud.estado = 'enviado-administrador'
+            try{
+                const solicitudCreada = crearSolicitud(nuevaSolicitud)
+                return solicitudCreada
+            }
+            catch(e)
+            {
+                console.log('error al crear la solicitud')
+            }
+
         }
     }
     return daoSolicitud
