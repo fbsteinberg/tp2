107242bf60efa734befe672ef0fa598b155c886c
gonzzj <gonzalojusid@gmail.com>
2021-06-21 17:12:47 -0300

    Fixed test cu

--- test/testEncolarCliente.js
+++ test/testEncolarCliente.js
@@ -7,7 +7,7 @@ const servidor = crearServidor();
 const port = getServerPort();
 await servidor.conectar(port);
 
-const { data } = await axios.post(`http://localhost:${port}/cola`, { idCliente: 0, idLocal: 0 });
+const { data } = await axios.post(`http://localhost:${port}/api/cola`, { idCliente: 0, idLocal: 0 });
 console.log(data);
 
 await servidor.desconectar();
